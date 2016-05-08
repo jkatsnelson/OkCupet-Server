4b24cae3f1e2b8b2b6d0d8f6baed3cfd0e5ec3b6
John Katsnelson <voltalocke@gmail.com>
2016-05-07 18:41:15 -0700

    adds webpack and typings

---
+++ src/app.ts
@@ -0,0 +1,16 @@
+/// <reference path="../typings/main.d.ts" />
+
+import * as express from "express";
+var app = express();
+
+app.get('/', function(req, res) {
+  res.send('Hello World!');
+});
+
+var server = app.listen(3000, function() {
+  var host: string = server.address().address;
+  var port: number = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+  console.log('congrats!');
+});
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+var path = require('path');
+var fs = require('fs');
+
+var nodeModules = {};
+fs.readdirSync('node_modules')
+  .filter(function(x) {
+    return ['.bin'].indexOf(x) === -1;
+  })
+  .forEach(function(mod) {
+    nodeModules[mod] = 'commonjs ' + mod;
+  });
+
+module.exports = {
+  entry: './src/app.ts',
+  target: 'node',
+  output: {
+    filename: 'bundle.js'
+  },
+  resolve: {
+    // Add `.ts` and `.tsx` as a resolvable extension.
+    extensions: ['', '.webpack.js', '.web.js', '.ts', '.tsx', '.js']
+  },
+  module: {
+    loaders: [
+      // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
+      { test: /\.tsx?$/, loader: 'ts-loader' },
+      { test: /\.json$/, loader: 'json-loader'}
+    ]
+  },
+  node: {
+    net: "empty",
+    fs: "empty"
+  },
+  externals: nodeModules
+}
